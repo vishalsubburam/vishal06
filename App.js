3d02ea987029bee0a0c3d3c3ecaffc2fc6c8f4be
vishalsubburam <71023269+vishalsubburam@users.noreply.github.com>
2022-04-26 16:45:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from "./Components/Dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
